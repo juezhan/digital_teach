fb2f2dd763b0befecd78bd87966a0df258dd6a67
sunlingfeng <minikiller@qq.com>
2017-07-27 15:12:27 +0800

    remove vue warning.

--- src/comm/InputCell/InputCell.vue
+++ src/comm/InputCell/InputCell.vue
@@ -3,15 +3,15 @@
     <el-input v-if="itemCell.type=='text'" v-model="inputValue" placeholder="请输入内容"></el-input>
     <el-select v-if="itemCell.type=='select'" v-model="inputValue">
       <el-option value="-1" label="请选择"></el-option>
-      <el-option v-for="item in itemCell.options" :label="item.text" :value="item.value"></el-option>
+      <el-option v-for="item in itemCell.options" :label="item.text" :value="item.value" :key="item.id"></el-option>
     </el-select>
     <el-switch v-if="itemCell.type=='switch'" on-text="ON" off-text="关" v-model="inputValue"></el-switch>
     <el-checkbox-group v-if="itemCell.type=='checkboxGroup'" v-model="inputValue">
       <el-checkbox v-for="item in itemCell.options" :label="item.text" :value="item.value"
-                   :key="item" :name="itemCell.itemName"></el-checkbox>
+                   :key="item.id" :name="itemCell.itemName"></el-checkbox>
     </el-checkbox-group>
     <el-radio-group v-if="itemCell.type=='radioGroup'" v-model="inputValue">
-      <el-radio v-for="item in itemCell.options" :label="item.text" :value="item.value" :key="item"
+      <el-radio v-for="item in itemCell.options" :label="item.text" :value="item.value" :key="item.id"
                 :name="itemCell.itemName"></el-radio>
     </el-radio-group>
     <el-input v-if="itemCell.type=='textarea'" type="textarea" v-model="inputValue"></el-input>
